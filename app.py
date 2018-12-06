d8a455c2bb63e402ae1bfbd44ad94f8671149b96
bobkoobg <boyko.surlev@gmail.com>
2018-12-06 23:27:03 +0100

    Testing

--- app.py
+++ app.py
@@ -6,44 +6,9 @@ app = Flask(__name__,
 )
 
 @app.route('/')
-def index():
-    return 'Index Page'
-
-@app.route('/helloalpha')
-def helloalpha():
-    return 'Hello, World'
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % username
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
-
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return 'Subpath %s' % subpath
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    print(">")
-    print(request.json)
-    print(request.args.get('bob'))
-
-    if request.method == 'POST':
-        return json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}])
-    else:
-        return "Show the login form"
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
+@app.route('/<name>')
 def hello(name=None):
-    print(request.cookies.get('username'))
-
+    # print(request.cookies.get('username'))
     resp = make_response(
         render_template(
             'hello.html',
@@ -54,16 +19,14 @@ def hello(name=None):
     resp.set_cookie('usernametwo', 'theusername')
     return resp
 
-# @app.route('/blobl')
-# def index():
-#     return redirect(url_for('/hello/'))
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    print(request.json)
+    print(request.args.get('bob'))
 
-
-with app.test_request_context():
-    url = url_for('static', filename='style.css')
-    print(url)
-#python -m flask run
-#set FLASK_ENV=development / production (in terminal)
-#set SERVER_NAME=bobbycom
+    if request.method == 'POST':
+        return json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}])
+    else:
+        return "Show the login form"
 
 app.run(debug=True)
